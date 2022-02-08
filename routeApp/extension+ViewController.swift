7388e3d6601e078153b09bb3578b84d82c468331
zalarudo <8339157@gmail.com>
2022-02-08 19:18:18 +0300

    CHANGE - removed storyboard ADD - mapkit & corelocation ADD - map on main view

---
+++ routeApp/extension+ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  extension+ViewController.swift
+//  routeApp
+//
+//  Created by Андрей Чистяков on 08.02.2022.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+extension ViewController{
+    
+    func setConstraints() {
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+         
+             
+        ])
+    }
+    
+}
