62acf101e6cae272e9b31d6922b2be109b942cbb
zalarudo <8339157@gmail.com>
2022-02-08 18:27:24 +0300

    Initial Commit

---
+++ routeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  routeApp
+//
+//  Created by Андрей Чистяков on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
